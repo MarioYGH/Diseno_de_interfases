26e902436f8e349867c7a55ef2604334acf5bc55
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-08-12 09:17:43 -0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 n = 11
-x = 0
-y = 1
+x = 0 #primer numero
+y = 1 #segundo numero
 
 for i in range(1,n):
-    z = y + x
-    y = x
-    x = z
+    z = y + x #suma de los dos numeros
+    y = x #cambias el valor del segundo numero por el que tenia el primero
+    x = z #asignas el valor de de z al primer numero 
     
     print(x)
     
