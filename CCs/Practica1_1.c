8d7f4a402a391dd5cd49f161b7e2fe8c7e5fa584
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-08-20 15:26:55 -0600

    Create Practica1_1.c

---
+++ CCs/Practica1_1.c
@@ -0,0 +1,27 @@
+#include <18F4550.h>
+#fuses XT,NOPROTECT,NOWDT,NOBROWNOUT,PUT,NOLVP 
+#use delay(clock=4000000)
+
+#BYTE PORTB = 0xF81
+
+void main(){
+   set_tris_b(0x00); 
+   //set_tris_d(0x0F); 
+   PORTB=0;                         //Comienza con todos los Leds Apagados
+   WHILE(TRUE)                      //Haga por siempre ....
+   {
+      PORTB=(0x01);                //Prendo unicamente el led de RB0
+      DELAY_MS(100);                //Retardo de 500 milisegundos
+      WHILE (!BIT_TEST(PORTB,7))    //Haga mientras el LED RB7 se encuentre apagado
+      {
+         PORTB=PORTB<<1;            //Rote hacia la izquierda una unidad
+         DELAY_MS(100);             //Retardo de 500 milisegundos
+      }
+      DELAY_MS(100);                //Retardo de 500 milisegundos
+      WHILE (!BIT_TEST(PORTB,0))    //Haga mientras el LED RB0 se encuentre apagado
+      {
+         PORTB=PORTB>>1;            //Rote hacia la derecha una unidad
+         DELAY_MS(100);             //Retardo de 500 milisegundos
+      }
+   }
+}
