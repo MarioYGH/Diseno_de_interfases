2fb776b8be466321bbaadf3baf04dce3548d6977
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-08-21 22:54:22 -0600

    Update Practica1_1.c

--- CCs/Practica1_1.c
+++ CCs/Practica1_1.c
@@ -8,20 +8,20 @@ void main(){
    set_tris_b(0x00); 
    //set_tris_d(0x0F); 
    PORTB=0;                         //Comienza con todos los Leds Apagados
-   WHILE(TRUE)                      //Haga por siempre ....
+   WHILE(TRUE)                     
    {
       PORTB=(0x01);                //Prendo unicamente el led de RB0
-      DELAY_MS(100);                //Retardo de 500 milisegundos
+      DELAY_MS(100);                //Delay
       WHILE (!BIT_TEST(PORTB,7))    //Haga mientras el LED RB7 se encuentre apagado
       {
          PORTB=PORTB<<1;            //Rote hacia la izquierda una unidad
-         DELAY_MS(100);             //Retardo de 500 milisegundos
+         DELAY_MS(100);             
       }
-      DELAY_MS(100);                //Retardo de 500 milisegundos
+      DELAY_MS(100);                
       WHILE (!BIT_TEST(PORTB,0))    //Haga mientras el LED RB0 se encuentre apagado
       {
          PORTB=PORTB>>1;            //Rote hacia la derecha una unidad
-         DELAY_MS(100);             //Retardo de 500 milisegundos
+         DELAY_MS(100);             
       }
    }
 }
