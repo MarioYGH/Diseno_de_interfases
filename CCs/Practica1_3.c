a17e49628692ac1e749930c6233af90de9ab4456
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-08-22 11:42:50 -0600

    Create Practica1_3.c

---
+++ CCs/Practica1_3.c
@@ -0,0 +1,70 @@
+#include <18F4550.h>
+#fuses XT,NOPROTECT,NOWDT,NOBROWNOUT,PUT,NOLVP 
+#use delay(clock=4000000)
+#use fast_io(B)
+#use fast_io(D)
+
+#byte portb = 0xF81
+#byte portd = 0xF83
+
+
+void main(){
+set_tris_B(0x00); 
+set_tris_D(0xFF); 
+portb = 0;
+portd = 0;
+int op;
+
+while(TRUE){ 
+op = input_d();
+switch(op){
+
+case 0x00:
+portb = 0;
+break;
+
+case 0x01:
+portb = 0;
+output_b(0x01); //UNO
+break;
+
+case 0x02:
+portb = 0;
+output_b(0x02); //DOS
+break;
+
+case 0x03:
+portb = 0;
+output_b(0x04); //TRES 
+break;
+
+case 0x04:
+portb = 0;
+output_b(0x08); //CUATRO 
+break;
+
+case 0x05:
+portb = 0;
+output_b(0x10); //CINCO
+break;
+
+case 0x06:
+portb = 0;
+output_b(0x20); //SEIS
+break;
+
+case 0x07:
+portb = 0;
+output_b(0x40); //SIETE
+break;
+
+case 0x08:
+portb = 0;
+output_b(0x80); //OCHO
+break;
+
+
+  }
+ }
+}
+   
