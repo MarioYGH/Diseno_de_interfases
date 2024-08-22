9b9454a71bad658eb56fdd44190494d3de844f67
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-08-21 22:53:14 -0600

    Update Tarea_2.py

--- Tarea_2.py
+++ Tarea_2.py
@@ -1,11 +1,21 @@
- 
-def for2(i,j,k):
-    i = i
-    j = i+j
-    k = not(i and k) 
-    print (i)
-    for2(i,j,k)
-    return i
 
-for2(1,1,3)
- 
+# for sin usar for ni ninguna funcion
+
+#i inicio
+#j es el paso
+#k es la condicion
+def for2(i, j, k):
+    print(i)
+    i = i + j
+    return (i <= k and [for2(i, j, k)]) 
+
+for2(0, 2, 8)
+
+#Creo que así va con otra condicion xd
+# def for2(i, j, k, cond):
+#     print(i)
+#     i = i + j
+#     return (cond(i, k) and for2(i, j, k, cond)) 
+
+# def condition(x, y):
+#     return x == y
