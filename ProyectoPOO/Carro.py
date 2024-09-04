42df8df60c263bec916149729f593e5087dfad91
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-09-04 09:21:56 -0600

    Update Carro.py

--- ProyectoPOO/Carro.py
+++ ProyectoPOO/Carro.py
@@ -13,11 +13,15 @@ class Scene:
     def update_scene(self):
         # Establecer fondo según la hora del día
         self.draw_background()
-        
+
         # Dibujar sol si no es de noche
         if self.time_of_day != 'night':
             self.draw_sun()
-        
+
+        # Dibujar nubes si no es de noche
+        if self.time_of_day != 'night':
+            self.draw_clouds()
+
         # Dibujar carretera, edificios, carro
         self.draw_road()
         self.draw_buildings()
@@ -27,10 +31,10 @@ class Scene:
         if self.time_of_day == 'morning':
             self.img[:] = (135, 206, 235)  # Azul claro
         elif self.time_of_day == 'afternoon':
-            self.img[:] = (255, 165, 0)    # Rojizo
+            self.img[:] = (255, 165, 0)  # Rojizo
         elif self.time_of_day == 'night':
-            self.img[:] = (0, 0, 0)        # Negro
-            self.draw_stars(100)           # Dibujar estrellas
+            self.img[:] = (0, 0, 0)  # Negro
+            self.draw_stars(100)  # Dibujar estrellas
             # Opcionalmente, puedes dibujar una luna si quieres en la noche
             cv.circle(self.img, (rand.randint(100, 800), 90), rand.randint(60, 75), (128, 128, 128), -1)
 
@@ -73,19 +77,27 @@ class Scene:
                     i = 75
                 cv.rectangle(self.img, (self.position, 200 - i), (self.position + 150, 350), (156, 156, 156), -1)
                 # columna izquierda
-                cv.rectangle(self.img, (self.position + 37 - 10, 200 + 37 - 10), (self.position + 37 + 10, 200 + 37 + 10), (255, 255, 255), -1)
-                cv.rectangle(self.img, (self.position + 37 - 10, 350 - 37 - 10), (self.position + 37 + 10, 350 - 37 + 10), (255, 255, 255), -1)
+                cv.rectangle(self.img, (self.position + 37 - 10, 200 + 37 - 10),
+                             (self.position + 37 + 10, 200 + 37 + 10), (255, 255, 255), -1)
+                cv.rectangle(self.img, (self.position + 37 - 10, 350 - 37 - 10),
+                             (self.position + 37 + 10, 350 - 37 + 10), (255, 255, 255), -1)
                 # columna derecha
-                cv.rectangle(self.img, (self.position + 150 - 37 - 10, 200 + 37 - 10), (self.position + 150 - 37 + 10, 200 + 37 + 10), (255, 255, 255), -1)
-                cv.rectangle(self.img, (self.position + 150 - 37 - 10, 350 - 37 - 10), (self.position + 150 - 37 + 10, 350 - 37 + 10), (255, 255, 255), -1)
+                cv.rectangle(self.img, (self.position + 150 - 37 - 10, 200 + 37 - 10),
+                             (self.position + 150 - 37 + 10, 200 + 37 + 10), (255, 255, 255), -1)
+                cv.rectangle(self.img, (self.position + 150 - 37 - 10, 350 - 37 - 10),
+                             (self.position + 150 - 37 + 10, 350 - 37 + 10), (255, 255, 255), -1)
                 if self.height == 2:
-                    cv.rectangle(self.img, (self.position + 37 - 10, 200 - 75 + 37 - 10), (self.position + 37 + 10, 200 - 75 + 37 + 10), (255, 255, 255), -1)
-                    cv.rectangle(self.img, (self.position + 150 - 37 - 10, 200 - 75 + 37 - 10), (self.position + 150 - 37 + 10, 200 - 75 + 37 + 10), (255, 255, 255), -1)
+                    cv.rectangle(self.img, (self.position + 37 - 10, 200 - 75 + 37 - 10),
+                                 (self.position + 37 + 10, 200 - 75 + 37 + 10), (255, 255, 255), -1)
+                    cv.rectangle(self.img, (self.position + 150 - 37 - 10, 200 - 75 + 37 - 10),
+                                 (self.position + 150 - 37 + 10, 200 - 75 + 37 + 10), (255, 255, 255), -1)
 
-        # Crear y dibujar edificios
         Buildings(self.img, height=2).draw()
         Buildings(self.img).draw()
         Buildings(self.img).draw()
+        Buildings(self.img).draw()
+        Buildings(self.img, height=2).draw()
+        Buildings(self.img, height=2).draw()
 
     def draw_car(self):
         x, y = rand.randint(200, 600), 540
@@ -93,6 +105,21 @@ class Scene:
         cv.rectangle(self.img, (x, y), (x + width, y + height), (0, 0, 255), -1)  # Carro rojo
         cv.circle(self.img, (x + 20, y + height), 15, (128, 128, 128), -1)  # Rueda izquierda
         cv.circle(self.img, (x + width - 20, y + height), 15, (128, 128, 128), -1)  # Rueda derecha
+
+    def draw_clouds(self):
+        cloud_count = rand.randint(3, 6)  # Número aleatorio de nubes
+        for _ in range(cloud_count):
+            x = rand.randint(0, self.img.shape[1] - 200)
+            y = rand.randint(0, self.img.shape[0] // 2)
+            cloud_width = rand.randint(100, 200)
+            cloud_height = rand.randint(30, 60)
+            color = (255, 255, 255)  # Color blanco para las nubes
+
+            # Dibujar la nube como una serie de elipses
+            for _ in range(rand.randint(3, 6)):  # Variar el número de elipses para cada nube
+                ellipse_x = x + rand.randint(-30, 30)
+                ellipse_y = y + rand.randint(-20, 20)
+                cv.ellipse(self.img, (ellipse_x, ellipse_y), (cloud_width // 2, cloud_height // 2), 0, 0, 360, color, -1)
 
 # Crear y actualizar la escena con tiempo del día aleatorio
 img = np.zeros((600, 900, 3), dtype=np.uint8)
