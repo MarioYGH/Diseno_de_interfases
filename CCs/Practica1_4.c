a18517394d35014b3d932c14c0a4e7513b8c29ed
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-09-08 23:59:01 -0600

    Update Practica1_4.c

--- CCs/Practica1_4.c
+++ CCs/Practica1_4.c
@@ -1,3 +1,8 @@
+// Asigne un numero decimal a cada LED, por ejemplo, LED D1 número 0, LED D2 número 1, etc. Haga lo mismo para cada micro switch de la matriz de 3x3. Ahora,
+// diseñe un programa en lenguaje C que lea el micro switch que fue presionado y encienda el LED asociado. Noté que le faltará un LED (9 micro switch y 8 LEDs).
+// Cuando el micro switch sobrante sea presionado, el programa diseñado debe iniciar la secuencia descrita en el paso 2. Para detener la secuencia, el usuario
+// debe presionar nuevamente el switch sobrante. Cuando la secuencia este activa el programa no debe permitir que los otros botones funcionen.
+
 #include <16F877A.h>
 #fuses XT, NOWDT,NOPUT,NOLVP,NOPROTECT,BROWNOUT
 #use delay(clock=20000000) 
