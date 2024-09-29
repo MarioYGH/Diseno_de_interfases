af7f49bf5d523db266a39e1ee4ad7a3b8baae9a3
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-09-29 16:24:14 -0600

    Update Practica5_2.py

--- CCs/Practica5_2.py
+++ CCs/Practica5_2.py
@@ -1,3 +1,9 @@
+# Diseñe una interfaz gráfica que permita calcular la resistencia total, la corriente y el voltaje de un circuito compuesto por cinco resistores. La interfaz debe contener 6
+# objetos para ingresar datos (Ustedes eligen que tipo de Edit Field usarán), cinco para ingresar el valor de resistor, y uno para elegir el resistor donde se calculará la
+# corriente y el voltaje. Asimismo, la interfaz debe contener tres objetos para mostrar los resultados de la resistencia total, la corriente y el voltaje. A través de dos botones
+# el usuario podrá seleccionar si la configuración de los resistores es serie o paralelo. Para el caso serie considere que hay una fuente de voltaje de 5V alimentando el
+# circuito, y para el caso paralelo, una fuente de corriente de 2 A. Los resultados para ambas configuraciones tienen que mostrarse en los mismos objetos de visualización.
+
 import tkinter as tk
 from tkinter import messagebox
 
@@ -5,11 +11,11 @@ from tkinter import messagebox
 root = tk.Tk()
 
 # Nombre de la ventana del contenedor
-root.title("Ventana de prueba")
-root.resizable(True,True)
+root.title("Cálculo de Circuitos - Serie y Paralelo")
+root.resizable(False, False)
 
 # Creacion del frame principal
-miframe = tk.Frame(root, width=1200,height=600)
+miframe = tk.Frame(root, padx=20, pady=20)
 miframe.pack()
 
 # Variables
@@ -20,39 +26,42 @@ voltageind = tk.StringVar()
 corrienteind = tk.StringVar()
 resistencia_seleccionada = tk.StringVar(value="R1") 
 
+# Estilos comunes
+label_font = ("Arial", 12)
+entry_font = ("Arial", 12)
+
 # Resistencia 1
-label1 = tk.Label(miframe, text="Resistencia 1:")
+label1 = tk.Label(miframe, text="Resistencia 1:", font=label_font)
 label1.grid(row=0, column=0, sticky="e", padx=10, pady=10)
-entry1 = tk.Entry(miframe)
-entry1.grid(row=0, column=1, padx=10, pady=10)
+entry1 = tk.Entry(miframe, font=entry_font)
+entry1.grid(row=0, column=1, padx=10, pady=10, ipady=5)
 
 # Resistencia 2
-label2 = tk.Label(miframe, text="Resistencia 2:")
+label2 = tk.Label(miframe, text="Resistencia 2:", font=label_font)
 label2.grid(row=1, column=0, sticky="e", padx=10, pady=10)
-entry2 = tk.Entry(miframe)
-entry2.grid(row=1, column=1, padx=10, pady=10)
+entry2 = tk.Entry(miframe, font=entry_font)
+entry2.grid(row=1, column=1, padx=10, pady=10, ipady=5)
 
 # Resistencia 3
-label3 = tk.Label(miframe, text="Resistencia 3:")
+label3 = tk.Label(miframe, text="Resistencia 3:", font=label_font)
 label3.grid(row=2, column=0, sticky="e", padx=10, pady=10)
-entry3 = tk.Entry(miframe)
-entry3.grid(row=2, column=1, padx=10, pady=10)
+entry3 = tk.Entry(miframe, font=entry_font)
+entry3.grid(row=2, column=1, padx=10, pady=10, ipady=5)
 
 # Resistencia 4
-label4 = tk.Label(miframe, text="Resistencia 4:")
+label4 = tk.Label(miframe, text="Resistencia 4:", font=label_font)
 label4.grid(row=3, column=0, sticky="e", padx=10, pady=10)
-entry4 = tk.Entry(miframe)
-entry4.grid(row=3, column=1, padx=10, pady=10)
+entry4 = tk.Entry(miframe, font=entry_font)
+entry4.grid(row=3, column=1, padx=10, pady=10, ipady=5)
 
 # Resistencia 5
-label5 = tk.Label(miframe, text="Resistencia 5:")
+label5 = tk.Label(miframe, text="Resistencia 5:", font=label_font)
 label5.grid(row=4, column=0, sticky="e", padx=10, pady=10)
-entry5 = tk.Entry(miframe)
-entry5.grid(row=4, column=1, padx=10, pady=10)
+entry5 = tk.Entry(miframe, font=entry_font)
+entry5.grid(row=4, column=1, padx=10, pady=10, ipady=5)
 
 def sumarserie():
     try:
-        
         # Obtener los valores de las entradas
         num1 = float(entry1.get())
         num2 = float(entry2.get())
@@ -85,12 +94,10 @@ def sumarserie():
         corrienteind.set(str(c))
     
     except ValueError:
-        # None
         messagebox.showerror("Error", "Por favor ingresa solo números.")
         
 def sumarparalelo():
     try:
-        
         # Obtener los valores de las entradas
         num1 = float(entry1.get())
         num2 = float(entry2.get())
@@ -122,61 +129,52 @@ def sumarparalelo():
         voltageind.set(str(voltaje))
         corrienteind.set(str(c))
         
-    
     except ValueError:
-        # None
         messagebox.showerror("Error", "Por favor ingresa solo números.")
 
-# Boton Serie
-boton = tk.Button(miframe, text="Serie", width=10, command=sumarserie)
-boton.grid(row=5,column=1)
-
-# Boton Paralelo
-boton = tk.Button(miframe, text="Paralelo", width=10, command=sumarparalelo)
-boton.grid(row=5,column=2)
-
-# # Boton Resistencia
-# boton = tk.Button(miframe, text="Resistencia", width=10, command=sumarserie)
-# boton.grid(row=5,column=0)
-
-# # Menú desplegable para seleccionar la resistencia para Ley de Ohm
-# etiqueta_seleccion = tk.Label(miframe, text="Resistencia:")
-# etiqueta_seleccion.grid(row=5, column=0, padx=10, pady=10)
-
+# Menú desplegable para seleccionar la resistencia
+tk.Label(miframe, text="Seleccionar Resistencia:", font=label_font).grid(row=5, column=0, padx=10, pady=10)
 opciones_resistencias = ["R1", "R2", "R3", "R4", "R5"]
 menu_resistencias = tk.OptionMenu(miframe, resistencia_seleccionada, *opciones_resistencias)
-menu_resistencias.grid(row=5, column=0, padx=10, pady=10)
+menu_resistencias.grid(row=5, column=1, padx=10, pady=10)
+
+# Boton Serie
+boton = tk.Button(miframe, text="Serie", width=10, font=entry_font, command=sumarserie)
+boton.grid(row=6, column=1, pady=10, padx=10)
+
+# Boton Paralelo
+boton = tk.Button(miframe, text="Paralelo", width=10, font=entry_font, command=sumarparalelo)
+boton.grid(row=6, column=2, pady=10, padx=10)
 
 # Resistencia Total
-label6 = tk.Label(miframe, text="Resistencia Total:")
-label6.grid(row=6, column=0, sticky="e", padx=10, pady=10)
-entry6 = tk.Entry(miframe, textvariable=resultado, state='readonly')
-entry6.grid(row=6, column=1, padx=0, pady=10)
+label6 = tk.Label(miframe, text="Resistencia Total:", font=label_font)
+label6.grid(row=7, column=0, sticky="e", padx=10, pady=10)
+entry6 = tk.Entry(miframe, textvariable=resultado, font=entry_font, state='readonly')
+entry6.grid(row=7, column=1, padx=10, pady=10)
 
 # Voltaje Total
-label7 = tk.Label(miframe, text="Voltaje Total:")
-label7.grid(row=7, column=0, sticky="e", padx=10, pady=10)
-entry7 = tk.Entry(miframe, textvariable=voltage, state='readonly')
-entry7.grid(row=7, column=1, padx=0, pady=10)
+label7 = tk.Label(miframe, text="Voltaje Total:", font=label_font)
+label7.grid(row=8, column=0, sticky="e", padx=10, pady=10)
+entry7 = tk.Entry(miframe, textvariable=voltage, font=entry_font, state='readonly')
+entry7.grid(row=8, column=1, padx=10, pady=10)
 
 # Corriente Total
-label8 = tk.Label(miframe, text="Corriente Total:")
-label8.grid(row=8, column=0, sticky="e", padx=10, pady=10)
-entry8 = tk.Entry(miframe, textvariable=corriente, state='readonly')
-entry8.grid(row=8, column=1, padx=0, pady=10)
+label8 = tk.Label(miframe, text="Corriente Total:", font=label_font)
+label8.grid(row=9, column=0, sticky="e", padx=10, pady=10)
+entry8 = tk.Entry(miframe, textvariable=corriente, font=entry_font, state='readonly')
+entry8.grid(row=9, column=1, padx=10, pady=10)
 
-# Voltaje
-label7 = tk.Label(miframe, text="Voltaje:")
-label7.grid(row=9, column=0, sticky="e", padx=10, pady=10)
-entry7 = tk.Entry(miframe, textvariable=voltageind, state='readonly')
-entry7.grid(row=9, column=1, padx=0, pady=10)
+# Voltaje Individual
+label9 = tk.Label(miframe, text="Voltaje:", font=label_font)
+label9.grid(row=10, column=0, sticky="e", padx=10, pady=10)
+entry9 = tk.Entry(miframe, textvariable=voltageind, font=entry_font, state='readonly')
+entry9.grid(row=10, column=1, padx=10, pady=10)
 
-# Corriente
-label8 = tk.Label(miframe, text="Corriente:")
-label8.grid(row=10, column=0, sticky="e", padx=10, pady=10)
-entry8 = tk.Entry(miframe, textvariable=corrienteind, state='readonly')
-entry8.grid(row=10, column=1, padx=0, pady=10)
-
-
+# Corriente Individual
+label10 = tk.Label(miframe, text="Corriente:", font=label_font)
+label10.grid(row=11, column=0, sticky="e", padx=10, pady=10)
+entry10 = tk.Entry(miframe, textvariable=corrienteind, font=entry_font, state='readonly')
+entry10.grid(row=11, column=1, padx=10, pady=10)
 
 root.mainloop()
+
